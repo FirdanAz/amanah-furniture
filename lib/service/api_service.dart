2b584b451b716116c8b80d08f2f7b2130d0e3bb6
JavierGavra <javiergavra01@gmail.com>
2023-02-10 21:12:47 +0700

    feat: [JG] add login feat

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,21 +1,23 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'package:amanah_furniture/common/public_function.dart';
 import 'package:amanah_furniture/model/product_all_model.dart';
+import 'package:amanah_furniture/ui/widget/snackbar.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  final _baseUrl = "http://amanahfurniture.space/api";
 
   Future getALlProduct() async {
-    const url =
-        "http://amanahfurniture.space/api/produk";
+    const endPoint = '/produk';
+    final url = '$_baseUrl$endPoint';
 
     try {
       final response = await http.get(Uri.parse(url));
-
       print('status code : ${response.statusCode}');
       if (response.statusCode == 200) {
-        AllProduct model =
-        AllProduct.fromJson(json.decode(response.body));
+        AllProduct model = AllProduct.fromJson(json.decode(response.body));
         print(model);
         return model;
       } else {
@@ -26,4 +28,44 @@ class ApiService {
     }
   }
 
+  Future postLogin(BuildContext context, String email, String password) async {
+    const endPoint = '/login';
+    final url = '$_baseUrl$endPoint';
+    final body = {
+      'email': email,
+      'password': password,
+    };
+
+    try {
+      final response = await http.post(Uri.parse(url), body: body);
+      print('status code : ${response.statusCode}');
+      if (response.statusCode == 200) {
+        String token = json.decode(response.body)['access_token'];
+        print(token);
+        await PublicFunction.setToken(token);
+        return true;
+      } else {
+        showSnackBar(
+          context,
+          content: const Text('Email atau Password salah'),
+          color: Colors.red,
+        );
+        return false;
+      }
+    } on SocketException {
+      showSnackBar(
+        context,
+        content: const Text('Tidak koneksi Internet'),
+        color: Colors.red,
+      );
+      return false;
+    } on HttpException {
+      showSnackBar(
+        context,
+        content: const Text('HttpException'),
+        color: Colors.red,
+      );
+      return false;
+    }
+  }
 }
